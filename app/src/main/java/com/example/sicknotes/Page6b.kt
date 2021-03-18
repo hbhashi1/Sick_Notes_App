6ba336aded544c9a9fbcae293202240ca355bda8
calteck <chinedu.onochie@rockets.utoledo.edu>
2021-03-18 11:36:56 -0400

    Changes made: Addition of page 6 and its relationship to page 4

---
+++ app/src/main/java/com/example/sicknotes/Page6b.kt
@@ -0,0 +1,20 @@
+package com.example.sicknotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Page6b : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page6b)
+
+        val button3 = findViewById<Button>(R.id.button3)
+        button3.setOnClickListener {
+            val intent = Intent(this, Page6b::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
