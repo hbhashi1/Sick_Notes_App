75d69a29a40dfd78e8f3c2ee791bc2b0213aaded
calteck <chinedu.onochie@rockets.utoledo.edu>
2021-03-21 02:26:37 -0400

    Changes made thus far: Creation of page 8, changing all the back button to have a uniform id of "back" as opposed to "backbutton" or "button 7" etc Next step would be to enable the back button functionality

---
+++ app/src/main/java/com/example/sicknotes/page8.kt
@@ -0,0 +1,11 @@
+package com.example.sicknotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class page8 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page8)
+    }
+}
