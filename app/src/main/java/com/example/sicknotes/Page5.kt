39b770cfa88544d7a5d47223d3890ef8508c3ee5
hananali <hannanbarre.hb@gmail.com>
2021-03-21 21:31:49 -0400

    connecting page 5 to page 8

--- app/src/main/java/com/example/sicknotes/Page5.kt
+++ app/src/main/java/com/example/sicknotes/Page5.kt
@@ -1,11 +1,20 @@
 package com.example.sicknotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Page5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page5)
+
+        val sendToPhysician = findViewById<Button>(R.id.button7)   // app breaks right here
+        sendToPhysician.setOnClickListener {
+            val intent = Intent(this, page8::class.java)
+            startActivity(intent)
+
+        }
     }
 }
